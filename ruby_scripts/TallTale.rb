2563501f1aa2c675a71772dfff961ddfe4d514aa
Marcel Sands <marcel.sands84@gmail.com>
2014-05-01 16:48:10 -0400

    define custom classes on Tall Tale story

--- ruby_scripts/TallTale.rb
+++ ruby_scripts/TallTale.rb
@@ -11,3 +11,22 @@
 # development of an Interactive storytelling game.
 #
 #--------------------------------------------------
+
+class Screen
+	def cls
+		puts ("\n" * 25)
+		puts "\a"
+	end
+end
+
+class Tale
+
+	attr_accessor :monster, :villain, :obect, :place, :location
+
+	attr_accessor :P1, :P2, :P3, :P4
+
+	def tell_Story(paragraph)
+		puts paragraph
+	end
+
+end
