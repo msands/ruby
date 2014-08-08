77c087ce353f4192d61c82e2a62561077fe9f52b
Marcel Sands <marcel.sands84@gmail.com>
2014-08-08 16:25:24 -0400

    update analyzer

--- ruby_apps/first_app/analyzer.rb
+++ ruby_apps/first_app/analyzer.rb
@@ -1,4 +1,9 @@
 
-line_coun = 0
-File.open("text.txt").each { |line| line_count += 1 }
-puts line_count
+text = ''
+line_count = 0
+File.open("text.txt").each do |line|
+  line_count += 1
+  text << line
+end
+
+puts "#{line_count} lines"
