a50209a7f1bb396b31ef2615d2fb03385868aa94
Marcel Sands <marcel.sands84@gmail.com>
2014-08-16 01:25:00 -0400

    change code from class to struct

--- ruby_apps/second_app/adventure.rb
+++ ruby_apps/second_app/adventure.rb
@@ -6,22 +6,6 @@ class Dungeon
     @rooms = []
   end
 
-  class Player
-    attr_accessor :name, :location
-
-    def initialize(player_name)
-      @name = player_name
-    end
-  end
-
-  class Room
-    attr_accessor :reference, :name, :description, :connections
-
-    def initialize(reference, name, description, connections)
-      @reference = reference
-      @name = name
-      @description = description
-      @connections = connections
-    end
-  end
+  Player = Struct.new(:name, :location)
+  Room = Struct.new(:reference, :name, :description, :connection)
 end
