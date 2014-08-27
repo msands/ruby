cd33418b78881b77f293140bffedfa5e36d692e8
Marcel Sands <marcel.sands84@gmail.com>
2014-08-27 02:25:50 -0400

    finish adventure ruby game

--- ruby_apps/second_app/adventure.rb
+++ ruby_apps/second_app/adventure.rb
@@ -6,6 +6,66 @@ class Dungeon
     @rooms = []
   end
 
-  Player = Struct.new(:name, :location)
-  Room = Struct.new(:reference, :name, :description, :connection)
+  def add_room(reference, name, description, connections)
+    @rooms << Room.new(reference, name, description, connections)
+  end
+
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms.detect { |room| room.reference == reference }
+  end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
+
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
+
+  class Room
+  end
+
+  class Player
+    attr_accessor :name, :location
+
+    def initialize(player_name)
+      @name = player_name
+    end
+  end
+
+  class Room
+    attr_accessor :reference, :name, :description, :connections
+
+    def initialize(reference, name, description, connections)
+      @reference = reference
+      @name = name
+      @description = description
+      @connections = connections
+    end
+
+    def full_description
+      @name + "\n\nYou are in " + @description
+    end
+  end
 end
+
+# Create the main dungeon object
+my_dungeon = Dungeon.new("Fred Bloggs")
+
+# Add rooms to the dungeon
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", {:west => :smallcave })
+my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", {:east => :largecave })
+
+# Start the dungeon by placing the player in the large cave
+my_dungeon.start(:largecave)
