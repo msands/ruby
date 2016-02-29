b59b5cd00a184baa189f131b5a9ccfbf3be8539b
Marcel Sands <marcel.sands84@gmail.com>
2016-02-28 20:23:23 -0500

    modify directory structure

---
+++ pianobar-notify.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+trigger = ARGV.shift
+
+if trigger == 'songstart'
+  songinfo = {}
+
+  STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2)) }
+  `growlnotify -t "Now Playing" -m "#{songinfo['title']}\nby #{songinfo['artist']}"`
+end
+
+# full instructions located at
+# http://leeourand.com/2011/01/25/pianobar-on-mac-osx/
